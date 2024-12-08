95be99970d771df018c41e38ae145cd61239baf4
David Maor <126754136+DavidMaor32@users.noreply.github.com>
2024-12-08 20:04:06 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,12 +4,13 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo -v
 
-GAMES = "nsnake bastet 2048 cmatrix cbonsai sl"
-TOOLS = "make git gh fzf tree bat thefuck"
-UTILITIES = "neofetch mc ranger tmux lynx"
+GAMES = "nsnake bastet 2048 cmatrix cbonsai asciiquarium sl"
+TOOLS = "make git gh fzf tree bat thefuck nginx"
+UTILITIES = "neofetch mc ranger tmux lynx cal"
+LIBRARIES = "libusb sqlite3 psql"
 MUST_HAVE = "python3 zsh"
 
-sudo apt-get install $MUST_HAVE $GAMES $TOOLS $UTILITIES -y
+sudo apt-get install $MUST_HAVE $GAMES $TOOLS $UTILITIES $LIBRARIES -y
 sudo -v
 
 # java jdk23
@@ -22,3 +23,20 @@ echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.zshrc
 # node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 nvm install 20
+
+# docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
