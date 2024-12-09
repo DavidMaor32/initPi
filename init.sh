ec398635a198025d549e56c172bfde8c65b8d2d6
David Maor <126754136+DavidMaor32@users.noreply.github.com>
2024-12-09 09:36:34 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,6 +13,9 @@ MUST_HAVE = "python3 zsh"
 sudo apt-get install $MUST_HAVE $GAMES $TOOLS $UTILITIES $LIBRARIES -y
 sudo -v
 
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # java jdk23
 wget https://download.java.net/java/GA/jdk23/3c5b90190c68498b986a97f276efd28a/37/GPL/openjdk-23_linux-aarch64_bin.tar.gz
 sudo tar -xvzf openjdk-23_linux-aarch64_bin.tar.gz -C /opt
